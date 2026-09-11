9929e5418c5a8c49ecb838c73f9ec0ca2c49859f
santiago gamboa <san.formacion.profesional.03@gmail.com>
2026-09-11 00:33:09 -0500

    Primer cambio en el proyecto desde git

---
+++ configuracion.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+using namespace std;
+
+// Bitwise
+// modificar bits
+// construir valores
+
+// Algoritmos pseudo-aleatorios
+// Memoria dinamica
+
+// Tools
+unsigned short inputIntPositive(){
+    return 0;
+}
